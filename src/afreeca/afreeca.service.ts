eeed77a6990dbf5b61d6adcd30c295ee628d2fa1
wei756 <wei756fg@gmail.com>
2024-04-16 16:13:45 +0900

    afreeca 요청시 method params 추가

--- src/afreeca/afreeca.service.ts
+++ src/afreeca/afreeca.service.ts
@@ -1,7 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Inject, Injectable } from '@nestjs/common';
-import { lastValueFrom, map } from 'rxjs';
-import { Station } from './afreeca.type';
+import { AxiosResponse } from 'axios';
+import { lastValueFrom, map, Observable } from 'rxjs';
 import { RequestAfreecaDto } from './dto/request-afreeca.dto';
 
 @Injectable()
@@ -13,13 +13,19 @@ export class AfreecaService {
 
   async request<T>(dto: RequestAfreecaDto): Promise<T> {
     const hostname = dto.hostname ?? this.hostname;
+    const requestParams = [
+      hostname + dto.pathname,
+      {
+        params: dto.params,
+        headers: { 'User-Agent': 'Mozilla/5.0' },
+      },
+    ];
+    const requestFunction =
+      this.httpService[dto.method?.toLowerCase() ?? 'get'];
     return await lastValueFrom(
-      this.httpService
-        .get(hostname + dto.pathname, {
-          params: dto.params,
-          headers: { 'User-Agent': 'Mozilla/5.0' },
-        })
-        .pipe(map((response) => response.data)),
+      (
+        requestFunction(...requestParams) as Observable<AxiosResponse<T, any>>
+      ).pipe(map((response) => response.data)),
     );
   }
 
--- src/afreeca/dto/request-afreeca.dto.ts
+++ src/afreeca/dto/request-afreeca.dto.ts
@@ -1,4 +1,12 @@
 export class RequestAfreecaDto {
+  readonly method?:
+    | 'GET'
+    | 'POST'
+    | 'PUT'
+    | 'DELETE'
+    | 'PATCH'
+    | 'HEAD'
+    | 'OPTIONS' = 'GET';
   readonly hostname?: string;
   readonly pathname: string;
   readonly params?: Record<string, string>;
