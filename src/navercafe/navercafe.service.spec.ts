bae53925749199ef88f7c918f9424cb79ecc1acb
wei756 <wei756fg@gmail.com>
2024-04-25 17:33:40 +0900

    navercafe 테스트 코드 추가

--- src/navercafe/navercafe.service.spec.ts
+++ src/navercafe/navercafe.service.spec.ts
@@ -17,12 +17,22 @@ describe('NavercafeService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-  
+
   it('get popular articles from navercafe', async () => {
     expect(await service.getPopularArticles()).toBeInstanceOf(Array);
   });
 
   it('get menus from navercafe', async () => {
     expect(await service.getMenus()).toBeInstanceOf(Array);
+  });
+
+  it('get article list from navercafe', async () => {
+    expect(
+      (await service.getArticleList({ menuId: 1 }))?.menuInfo?.menuId,
+    ).toBe(1);
+  });
+
+  it('get article from navercafe', async () => {
+    expect((await service.getArticle('3'))?.articleId).toBe(3);
   });
 });
