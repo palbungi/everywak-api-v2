2fc1f462472e9820d66f8c70a3c49be4d123f24a
wei756 <wei756fg@gmail.com>
2024-04-29 13:18:12 +0900

    bestwakki 모듈 테스트 코드

--- src/bestwakki/bestwakki.service.spec.ts
+++ src/bestwakki/bestwakki.service.spec.ts
@@ -1,7 +1,12 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from 'src/typeorm.config';
+import databaseConfig from 'src/config/database.config';
+import { TypeOrmConfigService } from 'src/config/typeorm.config';
+import youtubeConfig from 'src/config/youtube.config';
+import { NavercafeModule } from 'src/navercafe/navercafe.module';
 import { BestwakkiService } from './bestwakki.service';
+import { PopularArticle } from './entities/popular-article.entity';
 
 describe('BestwakkiService', () => {
   let service: BestwakkiService;
@@ -9,7 +14,22 @@ describe('BestwakkiService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
-        TypeOrmModule.forRoot(typeORMConfig),
+        ConfigModule.forRoot({
+          load: [databaseConfig, youtubeConfig],
+          envFilePath:
+            process.env.NODE_ENV === 'dev'
+              ? '.env.development.local'
+              : process.env.NODE_ENV === 'test'
+              ? '.env.test.local'
+              : '.env.production.local',
+          isGlobal: true,
+        }),
+        TypeOrmModule.forFeature([PopularArticle]),
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useClass: TypeOrmConfigService,
+        }),
+        NavercafeModule, 
       ],
       providers: [BestwakkiService],
     }).compile();
@@ -19,5 +39,9 @@ describe('BestwakkiService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('인기글 갱신', async () => {
+    expect((await service.update()).identifiers).toBeInstanceOf(Array);
   });
 });
