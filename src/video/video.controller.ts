153a88cdd2f15b39318f259d45bee428a6733db0
wei756 <wei756fg@gmail.com>
2024-04-29 19:26:05 +0900

    video cron 설정

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { SearchVideoDto } from './dto/search-video.dto';
 import { VideoService } from './video.service';
 
@@ -15,6 +16,12 @@ export class VideoController {
 
   @Get('update')
   update() {
+    return this.videoService.updateVideos();
+  }
+
+  // 하루에 네 번 갱신
+  @Cron('0 0,6,12,18 * * *')
+  updateVideosCron() {
     return this.videoService.updateVideos();
   }
 }
