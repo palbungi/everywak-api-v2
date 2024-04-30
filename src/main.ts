92057aab1f1d7e0b15100504959bb921b52b18fa
wei756 <wei756fg@gmail.com>
2024-05-01 04:52:11 +0900

    ssl 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { readFileSync } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const ssl = process.env.SSL === 'true' ? true : false;
+  let httpsOptions = null;
+  if (ssl) {
+    httpsOptions = {
+      key: readFileSync(process.env.SSL_KEY_PATH),
+      cert: readFileSync(process.env.SSL_CERT_PATH),
+      ca: readFileSync(process.env.SSL_CA_PATH),
+      minVersion: 'TLSv1.2',
+    };
+  }
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
