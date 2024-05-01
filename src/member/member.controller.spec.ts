a2871db3d719ee4d8d760838472a71e640039cba
wei756 <wei756fg@gmail.com>
2024-05-01 20:59:37 +0900

    member 테스트 코드 수정

--- src/member/member.controller.spec.ts
+++ src/member/member.controller.spec.ts
@@ -1,63 +1,74 @@
-import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import databaseConfig from 'src/config/database.config';
-import { TypeOrmConfigService } from 'src/config/typeorm.config';
-import youtubeConfig from 'src/config/youtube.config';
-import { LiveChange } from 'src/live/entities/live-change.entity';
-import { Live } from 'src/live/entities/live.entity';
-import { OBI } from 'src/obi/entities/obi.entity';
-import { YoutubeModule } from 'src/youtube/youtube.module';
-import { DataSource } from 'typeorm';
-import { LivePlatform } from './entities/livePlatform.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { YoutubeService } from 'src/youtube/youtube.service';
+import { Repository } from 'typeorm';
 import { Member } from './entities/member.entity';
-import { Profile } from './entities/profile.entity';
-import { Social } from './entities/social.entity';
-import { YoutubeChannel } from './entities/youtubeChannel.entity';
 import { MemberController } from './member.controller';
 import { MemberService } from './member.service';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('MemberController', () => {
   let controller: MemberController;
-  let dataSource: DataSource;
+  let memberRepository: MockRepository<Member>;
+  let youtubeService = {
+    getChannel: jest.fn().mockResolvedValue({
+      id: 'id',
+      title: 'title',
+      description: 'description',
+      customUrl: 'customUrl',
+      publishedAt: 'publishedAt',
+      thumbnails: {
+        default: {
+          url: 'url',
+          width: 1,
+          height: 1,
+        },
+        medium: {
+          url: 'url',
+          width: 1,
+          height: 1,
+        },
+        high: {
+          url: 'url',
+          width: 1,
+          height: 1,
+        },
+      },
+      country: 'country',
+      viewCount: 1,
+      subscriberCount: 1,
+      hiddenSubscriberCount: true,
+      videoCount: 1,
+      keywords: 'keywords',
+      topicIds: ['topicIds'],
+      topicCategories: ['topicCategories'],
+    }),
+  };
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          load: [databaseConfig, youtubeConfig],
-          envFilePath: '.env.test.local',
-          isGlobal: true,
-        }),
-        TypeOrmModule.forRootAsync({
-          imports: [ConfigModule],
-          useClass: TypeOrmConfigService,
-        }),
-        TypeOrmModule.forFeature([
-          Member,
-          Profile,
-          LivePlatform,
-          Social,
-          YoutubeChannel,
-          OBI,
-          Live,
-          LiveChange,
-        ]),
-        YoutubeModule,
-      ],
       controllers: [MemberController],
-      providers: [MemberService],
+      providers: [MemberService,
+        {
+          provide: getRepositoryToken(Member),
+          useValue: {
+            find: jest.fn(),
+          },
+        },
+        {
+          provide: YoutubeService,
+          useValue: youtubeService,
+        },],
     }).compile();
 
     controller = module.get<MemberController>(MemberController);
-    dataSource = module.get<DataSource>(DataSource);
+    memberRepository = module.get<MockRepository<Member>>(
+      getRepositoryToken(Member),
+    );
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
-  });
-
-  afterAll(async () => {
-    await dataSource.destroy();
   });
 });
