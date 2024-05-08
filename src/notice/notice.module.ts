209a7c62c117ecc9e5937238694e275ec60c1abc
wei756 <wei756fg@gmail.com>
2024-05-08 21:41:08 +0900

    notice 누락 import 추가

--- src/notice/notice.module.ts
+++ src/notice/notice.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MemberModule } from 'src/member/member.module';
 import { NavercafeModule } from 'src/navercafe/navercafe.module';
+import { MemberNotice } from './entities/member-notice.entity';
 import { NoticeController } from './notice.controller';
 import { NoticeService } from './notice.service';
 
 @Module({
-  imports: [NavercafeModule, MemberModule],
+  imports: [
+    TypeOrmModule.forFeature([MemberNotice]),
+    NavercafeModule,
+    MemberModule,
+  ],
   controllers: [NoticeController],
   providers: [NoticeService],
 })
