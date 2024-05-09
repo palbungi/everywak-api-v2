75965f6d6d1bb97823e446949c7b4e89aa960381
wei756 <wei756fg@gmail.com>
2024-05-09 16:25:41 +0900

    notice controller 구현

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { SearchMemberNoticeDto } from './dto/search-member-notice.dto';
+import { NoticeService } from './notice.service';
 
 @Controller('notice')
-export class NoticeController {}
+export class NoticeController {
+  constructor(private readonly noticeService: NoticeService) {}
+
+  @Get('/list')
+  list(@Query() dto: SearchMemberNoticeDto) {
+    return this.noticeService.find(dto);
+  }
+
+  @Get('/update')
+  update() {
+    return this.noticeService.update();
+  }
+
+  @Cron('0 */5 * * * *')
+  updateCron() {
+    return this.noticeService.update();
+  }
+}
